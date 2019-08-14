6c0c3c7aecfad5cf067f941865db6c997f87ea84
Piotr Bartoszewski <bartoszewskip1000@gmail.com>
2019-08-14 15:24:36 +0200

    Add code refactor, smooth camera follow, fixed many bugs. Game works really fine.

--- Assets/Scripts/Ally.cs
+++ Assets/Scripts/Ally.cs
@@ -5,70 +5,121 @@ using UnityEngine;
 
 public class Ally : MonoBehaviour
 {
-    Animator animator;
-    Rigidbody rb;
+    #region Ally Groups
+    public enum AllyRank
+    {
+        Boss, Member
+    }
+    /// <summary>
+    /// If it is not equal to null, I am an ally
+    /// </summary>
+    public AllyRank? myAllyRank = null;
+    
+    /// <summary>
+    /// Static reference to the boss of allies, but can only be used if myAllyRank != null
+    /// </summary>
+    public static Ally boss;
+    #endregion
 
-    Vector3 terrainSize;
+    Animator myAnimator;
+    Rigidbody myRB;
 
+    #region Swipes
     Vector2 startFingerPos;
     Vector2 endFingerPos;
     Vector2 deltaFingerPos;
-
-    GameObject observationObj;
+    #endregion
 
     void Start()
     {
-        animator = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody>();
-        terrainSize = FindObjectOfType<Terrain>().terrainData.size;
+        if (name == "Cat")
+            myAllyRank = AllyRank.Boss;
+        myAnimator = GetComponent<Animator>();
+        myRB = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        if (observationObj != null && Vector3.Distance(transform.position, observationObj.transform.position) > 3)
+        try
         {
-            transform.LookAt(observationObj.transform);
-            transform.Translate(Vector3.forward * observationObj.GetComponent<Rigidbody>().velocity.magnitude * Time.deltaTime);
-        }
-
-        if(name == "Cat")
-        {
-            if (rb.velocity.y < 5)
-                rb.velocity = new Vector3(rb.velocity.x, 0, 5);
-            foreach (var touch in Input.touches)
+            if (myAllyRank == AllyRank.Boss)
             {
-                if (touch.phase == TouchPhase.Began)
+                boss = this;
+                //my speed cannot be lower than 5
+                if (myRB.velocity.z < 5)
+                    myRB.velocity = new Vector3(myRB.velocity.x, 0, 5);
+
+                foreach (var touch in Input.touches)
                 {
-                    startFingerPos = touch.position;
-                    CancelInvoke();
-                }
+                    //on swipe start save touch pos
+                    if (touch.phase == TouchPhase.Began)
+                    {
+                        startFingerPos = touch.position;
+                    }
 
-                if (touch.phase == TouchPhase.Ended)
-                {
-                    endFingerPos = touch.position;
+                    if (touch.phase == TouchPhase.Ended)
+                    {
+                        //when the swipe ends save the touch pos
+                        endFingerPos = touch.position;
 
-                    deltaFingerPos = endFingerPos - startFingerPos;
+                        //calculate the swipe length
+                        deltaFingerPos = endFingerPos - startFingerPos;
 
-                    Vector3 playerDir = Vector3.zero;
+                        //if the swipe isn't enough big, skip to next touch
+                        if (deltaFingerPos.magnitude < 50)
+                            continue;
 
-                    playerDir.z = deltaFingerPos.y > 0 ? 0.5f : 0f;
-                    playerDir.x = deltaFingerPos.x > 0 ? 0.5f : -0.5f;
+                        Vector3 move = Vector3.zero;
 
-                    rb.velocity += playerDir * 10;
+                        move.z = deltaFingerPos.y > 0 ? 5f : 0f;
+                        move.x = deltaFingerPos.x > 0 ? 10f : -10f;
+                        //y is always 0
+
+                        myRB.velocity += move;
+                    }
                 }
             }
+            else if (myAllyRank == AllyRank.Member)
+            {
+                //check if the boss is alive
+                if(boss == null)
+                {
+                    myAllyRank = AllyRank.Boss;
+                    boss = this;
+                    transform.localEulerAngles = Vector3.zero; //reset the rotation (because of transform.LookAt)
+                }
+
+                if(Vector3.Distance(transform.position, boss.transform.position) > 3)
+                {
+                    transform.LookAt(boss.transform);
+                    Vector3 move = Vector3.forward * boss.GetComponent<Rigidbody>().velocity.magnitude;
+                    transform.Translate(move * Time.deltaTime);
+                }
+            }
+            else if (myAllyRank == null)
+            {
+                //I am not an ally yet
+            }
         }
+        catch (Exception e) { }
     }
-        
+
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player" && name != "Cat")
+        if (!other.GetComponent<Ally>()) //I am not a knight
+            return;
+        else
         {
-            tag = "Player";
-            //I have been staying on the road and "other" is the player collecting allies
-            /*transform.SetParent(other.transform.parent);
-            transform.localEulerAngles = Vector3.zero;*/
-            observationObj = other.gameObject;
+            //I am a knight and a potential ally
+            Ally otherAlly = other.GetComponent<Ally>();
+
+            if (otherAlly.myAllyRank != null && //otherAlly is a knight and an ally
+                myAllyRank == null) //I cannot be an ally
+            {
+                //I had been staying on the road and "other" is the player collecting allies
+                
+                myAllyRank = AllyRank.Member;
+            }
         }
     }
 }
---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public Vector3 offset = Vector3.zero;
+
+    void Update()
+    {
+        if(Ally.boss != null)
+        {
+            Vector3 targetPos = Ally.boss.transform.position + offset;
+            transform.position = Vector3.Lerp(transform.position, targetPos, 0.05f);
+        }
+    }
+}
